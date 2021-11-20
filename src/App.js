4d1e5880c5d9c3ed5ba2b5eb74e0950cce98ced2
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-11-20 17:17:14 -0300

    Usando HorizontalScroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import HorizontalScroll from "react-scroll-horizontal";
+import { Apps, Container, Container2, Container3 } from "../src/styled";
+
 
 function App() {
   return (
     <div>
-      <h1>Ecodesign</h1>
+      <Apps>
+        <HorizontalScroll reverseScroll={true} animValues={1}>
+          <Container>
+            <h1>Ecodesign</h1>
+          </Container>
+          <Container2>
+            <h1>Ecodesign</h1>
+          </Container2>
+          <Container3>
+            <h1>Ecodesign</h1>
+          </Container3>
+        </HorizontalScroll>
+      </Apps>
     </div>
   );
 }
